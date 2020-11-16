ebbcfa259dd9e0a01f98aef17fe7d0dfac705257
xizher <xizher@163.com>
2020-11-16 19:23:05 +0800

    feat: *

--- test/test-datastructure/test-datastructure.js
+++ test/test-datastructure/test-datastructure.js
@@ -1,6 +1,9 @@
 import {
 // Stack,
 // Queue,
+  Graph,
+  GraphDirected,
+  breadthFirstSearch,
 } from '../../datastructure'
 
 //#region 栈 测试
@@ -45,3 +48,26 @@ import {
 
 //#endregion
 
+//#region 图 测试
+// const graph = new GraphDirected()
+const graph = new Graph()
+let myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+myVertices.forEach((v) => {
+  graph.addVertex(v)
+})
+graph.addEdge('A', 'B')
+graph.addEdge('A', 'C')
+graph.addEdge('A', 'D')
+graph.addEdge('C', 'D')
+graph.addEdge('C', 'G')
+graph.addEdge('D', 'G')
+graph.addEdge('D', 'H')
+graph.addEdge('B', 'E')
+graph.addEdge('B', 'F')
+graph.addEdge('E', 'I')
+
+console.log(graph.toString())
+const result = breadthFirstSearch(graph, 'C')
+console.log(result)
+
+//#endregion
