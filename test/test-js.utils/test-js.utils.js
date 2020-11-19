353ce0c113dfe358e6a6c023e31c47b0ab565328
xizher <xizher@163.com>
2020-11-19 13:08:14 +0800

    test: 加密解密函数测试

--- test/test-js.utils/test-js.utils.js
+++ test/test-js.utils/test-js.utils.js
@@ -1,18 +1,23 @@
 import {
-  guid,
-  deepCopy,
-  // getRootPath,
-  randomRange,
+// guid,
+// deepCopy,
+// getRootPath,
+// randomRange,
+  enCrypto, deCrypto,
 } from '../../js.utils/index'
 
-console.log(guid())
-console.log(guid())
+// console.log(guid())
+// console.log(guid())
 
-const obj = { a: 1 }
-const obj2 = deepCopy(obj)
-obj2.a = 2
-console.log(obj, obj2)
+// const obj = { a: 1 }
+// const obj2 = deepCopy(obj)
+// obj2.a = 2
+// console.log(obj, obj2)
 
-// console.log(window && getRootPath()) // no windows
+// // console.log(window && getRootPath()) // no windows
 
-console.log(randomRange(2, 3), randomRange(2, 3))
+// console.log(randomRange(2, 3), randomRange(2, 3))
+
+console.log(enCrypto('test'))
+console.log(deCrypto(enCrypto('test')))
+
