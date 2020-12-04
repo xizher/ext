8e3d431e3774b6c8251c0b136b448e09a13d9b31
xizher <xizher@163.com>
2020-12-04 21:09:46 +0800

    test: *

--- test/test.node.js
+++ test/test.node.js
@@ -1,18 +1,70 @@
-const { readFileAsync, writeFileAsync, pgSql } = require('../nodejs')
+const { readFileAsync, writeFileAsync, pgSql, mongodbHelper, MongodbHelper } = require('../nodejs')
 
-readFileAsync('test/assets/test.txt').then(data => {
-  console.log(data)
-  writeFileAsync('test/assets/test.txt', new Date().getTime())
-})
+// readFileAsync('test/assets/test.txt').then(data => {
+//   console.log(data)
+//   writeFileAsync('test/assets/test.txt', new Date().getTime())
+// })
 
-pgSql.setConfig({
-  port: 5432,
+// pgSql.setConfig({
+//   port: 5432,
+//   host: '39.107.24.26',
+//   user: 'postgres',
+//   password: 'postgis',
+//   database: 'wuxizhefun'
+// })
+
+// pgSql.exec('select * from tb_money').then(res => {
+//   console.log(res.rows)
+// })
+
+console.log(mongodbHelper)
+
+mongodbHelper.setConfig({
+  port: 27017,
   host: '39.107.24.26',
-  user: 'postgres',
-  password: 'postgis',
-  database: 'wuxizhefun'
+  user: 'wxzgis',
+  pwd: 'wxzgis123',
+  db: 'wxzgis'
 })
 
-pgSql.exec('select * from tb_money').then(res => {
-  console.log(res.rows)
-})
+// mongodbHelper
+//   .useCollection('tb_account')
+//   .delete({ username: 'test' })
+//   .then(res => {
+//     console.log(res)
+//   })
+
+// mongodbHelper
+//   .useCollection('tb_account')
+//   .insert({ username: 'test' })
+//   .then(res => {
+//     console.log(res)
+//   })
+
+// mongodbHelper
+//   .useCollection('tb_account')
+//   .update({ username: 'test' }, { gender: 'y' })
+//   .then(res => {
+//     console.log(res)
+//   })
+
+// mongodbHelper
+//   .useCollection('tb_account')
+//   .query({ username: 'test' })
+//   .then(res => {
+//     console.log(res)
+//   })
+
+// mongodbHelper
+//   .useCollection('tb_account')
+//   .updateFromOid('5fca2f2c343d162d3817d393', { gender: 'x' })
+//   .then(res => {
+//     console.log(res)
+//   })
+
+mongodbHelper
+  .useCollection('tb_account')
+  .queryFromOid('5fca2f2c343d162d3817d393')
+  .then(res => {
+    console.log(res)
+  })
